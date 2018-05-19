b57685901d1ac79c2e18bdf774f242fc2b21af1a
mzg <15122302865@163.com>
2018-05-19 22:40:02 +0800

    fix #16 企业信息配置

--- jobplus/forms.py
+++ jobplus/forms.py
@@ -1,8 +1,41 @@
 # *_*coding:utf-8 *_*
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
-from wtforms.validators import Length, Email, EqualTo, Required
-from jobplus.models import db, User
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,ValidationError
+from wtforms import TextAreaField,IntegerField
+from wtforms.validators import Length,Email,EqualTo,Required,URL,NumberRange
+from wtforms import ValidationError
+from jobplus.models import User,JobInfo,ComInfo,db
+
+
+class CompanyForm(FlaskForm):
+    com_name = StringField('企业名称',validators=[Required(),Length(3,24)])
+    com_email = StringField('邮箱',validators=[Required(),Email()])
+    #password = StringField('密码',)
+    com_location = StringField('地址',validators=[Required(),Length(3,24)])
+    com_logo = StringField('logo链接',validators=[Required(),URL()])
+    com_web = StringField('网站链接',validators=[Required(),URL()])
+    com_desc_less = StringField('一句话简介',validators=[Required(),Length(3,48)])
+    com_desc_more = StringField('详细介绍',validators=[Required(),Length(3,256)])
+    submit = SubmitField('提交')
+    def set_details(self):
+        company = ComInfo()
+        
+        # 增加用户登录系统后，此处需要修改为当前用户的id，或者去掉
+        company.com_id=1
+        company.com_name = self.com_name.data
+        company.com_email = self.com_email.data
+        company.com_location = self.com_location.data
+        company.com_logo = self.com_logo.data
+        company.com_web = self.com_web.data
+        company.com_desc_less = self.com_desc_less.data
+        company.com_desc_more = self.com_desc_more.data
+        
+        db.session.add(company)
+        db.session.commit()
+        return company
+
+    # 需要验证邮箱唯一性？
 
 class RegisterForm(FlaskForm):
     username = StringField('用户名', validators=[Required(), Length(3, 24)])
--- jobplus/models.py
+++ jobplus/models.py
@@ -34,6 +34,7 @@ class User(Base,UserMixin):
     #password 指定列名
     _password = db.Column('password',db.String(256),nullable=False)
     role = db.Column(db.SmallInteger,default=ROLE_USER)
+    resume = db.Column(db.String(128))
     # 逻辑删除标记，若不用，可忽略,0-删除,1-未删除
     isdeleted = db.Column(db.SmallInteger,default=0)
 
@@ -97,9 +98,10 @@ class ComInfo(Base):
 
     com_name = db.Column(db.String(128))
     com_email = db.Column(db.String(64),unique=True,nullable=False)
-    com_phone = db.Column(db.Integer,unique=True,nullable=False)
+    com_phone = db.Column(db.Integer,unique=True)
     com_location = db.Column(db.String(128))
     com_logo = db.Column(db.String(128))
+    com_web = db.Column(db.String(64))
     com_desc_less = db.Column(db.String(128))
     com_desc_more = db.Column(db.String(256))
 
@@ -120,3 +122,6 @@ class UserJob(Base):
     # 与JobInfo表建立关系
     job_id = db.Column(db.Integer,db.ForeignKey('jobinfo.job_id'))
     job = db.relationship('JobInfo',backref=db.backref('interm'))
+
+    # 简历状态 0-待查看，1-已拒绝，2-面试
+    status = db.Column(db.SmallInteger,default=0)
